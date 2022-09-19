abdded024ba9495fec63a08836432cc1f475da90
Serhii Kuhaiev <kugaew@gmail.com>
2022-09-19 23:40:30 +0200

    Made Task-1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const itemsRef = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${itemsRef.length}`);
+
+//example-1
+for (let i = 0; i < itemsRef.length; i += 1) {
+    console.log(
+        `Category: ${itemsRef[i].children[0].textContent}\nElements: ${itemsRef[i].children[1].children.length}`
+    );
+}
+
+//example-2
+itemsRef.forEach(item => {
+    console.log(
+        `Category: ${item.children[0].textContent}\nElements: ${item.children[1].children.length}`
+    );
+});
