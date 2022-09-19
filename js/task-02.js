89f042b6246b7c504739b0738a65e1b3042e9e90
Serhii Kuhaiev <kugaew@gmail.com>
2022-09-19 23:59:48 +0200

    Made Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listRef = document.querySelector('#ingredients');
+
+const itemsRef = ingredients.map(ingredient => {
+    const itemRef = document.createElement('li');
+    itemRef.textContent = ingredient;
+    itemRef.classList = 'item';
+    return itemRef;
+});
+
+listRef.append(...itemsRef);
