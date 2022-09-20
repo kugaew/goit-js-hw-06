01b0d2a04900b4793b1a4824a5e1715159a92262
Serhii Kuhaiev <kugaew@gmail.com>
2022-09-20 17:55:10 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,14 @@
 const inputTextSizeRangeRef = document.querySelector('#font-size-control');
 const spanOutputTextValueSize = document.querySelector('#text');
 
-inputTextSizeRangeRef.addEventListener('input', () => {
-    spanOutputTextValueSize.style.fontSize = `${inputTextSizeRangeRef.value}px`;
-});
+//exampel-1
+/* inputTextSizeRangeRef.addEventListener('input', event => {
+    spanOutputTextValueSize.style.fontSize = `${event.currentTarget.value}px`;
+}); */
+
+//exampel-2
+inputTextSizeRangeRef.addEventListener('input', onInputTextSizeChange);
+
+function onInputTextSizeChange(event) {
+    spanOutputTextValueSize.style.fontSize = `${event.currentTarget.value}px`;
+}
